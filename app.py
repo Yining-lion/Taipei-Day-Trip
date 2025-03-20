28f2e5b0425484dc87888473a7f8638361e2bd8b
unknown <resurrection2023y@gmail.com>
2025-03-20 22:06:35 +0800

    Finish part2

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import *
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 import mysql.connector.pooling
 from typing import Annotated
 import json
@@ -36,19 +37,23 @@ async def getAttractions(
 		offset = data_count * page
 		cursor.execute(base_sql + "LIMIT %s OFFSET %s",(data_count, offset))
 		result = cursor.fetchall()
+		# 計算 table 總數量
+		cursor.execute("SELECT COUNT(*) FROM taipei_attractions")
+		table_len = cursor.fetchone()["COUNT(*)"]
 
 		# 處理 keyword：用來完全比對捷運站名稱、或模糊比對景點名稱的關鍵字，沒有給定則不做篩選
 		if keyword:
 			cursor.execute(base_sql + "WHERE mrt = %s OR name LIKE %s LIMIT %s OFFSET %s",(keyword, f"%{keyword}%", data_count, offset))
 			result = cursor.fetchall()
+			# 計算符合 keyword 條件的總數量
+			cursor.execute("SELECT COUNT(*) FROM taipei_attractions WHERE mrt = %s OR name LIKE %s", (keyword, f"%{keyword}%"))
+			table_len = cursor.fetchone()["COUNT(*)"]
 
 		# 解析 images JSON 格式
 		for row in result:
 			row["images"] = json.loads(row["images"])
 		
 		# 處理 nextPage
-		cursor.execute("SELECT COUNT(*) FROM taipei_attractions")
-		table_len = cursor.fetchone()["COUNT(*)"]
 		next_page = page + 1
 		if data_count * (page+1) >= table_len:
 			next_page = None
@@ -100,6 +105,8 @@ async def getMrts(request: Request,):
 	finally:
 		cursor.close()
 		cnx.close()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Static Pages (Never Modify Code in this Block)
 @app.get("/", include_in_schema=False)
